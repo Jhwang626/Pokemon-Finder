95821d1d8544e768fc695c973b4bc9bdc68c6755
Jhwang626 <133681625+Jhwang626@users.noreply.github.com>
2023-06-02 15:43:21 +0000

    testing....

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import urllib.request
 import json
-import random
+import requests
+url = "https://pokeapi.co/api/v2/"
 
-url = "https://pokeapi.co/api/v2/?"
+trace ("Calling", URL)
+response = requests.get(URL) # Get data from the URL
+response.raise_for_status()
+
+data = response.json()
+trace ("\nText returned:", response.text)
+
+trace ("\nHere are all the kay/value pairs in the JSON response:")
+for key, value in data.items():
+  trace (key, ": ", value)
+
+print(f"Here's a couple choices:{data['activity']}")
+
 
 
 
